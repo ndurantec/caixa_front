97e8f3f94f32fba721088776de2a83eccb94f347
LckzUp <lucas.pinto17@escola.pr.gov.br>
2024-09-06 11:57:41 -0300

    atualizacao geral do fluxo e conta inteiro]

--- script/conta.js
+++ script/conta.js
@@ -1,17 +1,195 @@
-var meuFormulario = document.getElementById("conta_colegio");
-meuFormulario.addEventListener("submit", function(event) {
-  event.preventDefault();
-  cadastrar_conta();
-});
+function salvar() {
+  const nome_conta = document.getElementById('nome_conta').value;
+  const descricao_conta = document.getElementById('descricao_conta').value;
 
-function capturarNomeConta() {
-  const nome_conta = Number(document.getElementById("nome_conta").value);
-  return nome_conta;
+  console.log(nome_conta)
+  console.log(descricao_conta)
+ 
+  // Cabeçalho não visivel para o usuario
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  fetch('localhost:8080/fluxo/inserir' ,{
+
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+   
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: nome_conta }),
+
+    headers: headers
+
+    //Aqui inicia função then
+  }).then(response => {
+
+    if(response.ok) {
+
+      //Esta linha imprime a mensagem no concole
+      console.log('Foi no servidor e voltou');
+
+      //Esta linha carrega a página sucesso
+      window.location.href = 'sucesso.html'    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
+
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+   
+
+}
+   
+function consultar() {
+  const nome_conta = document.getElementById('nome_conta').value;
+  const descricao_conta = document.getElementById('descricao_conta').value;
+
+  console.log(nome_conta)
+  console.log(descricao_conta)
+ 
+  // Cabeçalho não visivel para o usuario
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  fetch('localhost:8080/fluxo/inserir' ,{
+
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+   
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: nome_conta }),
+
+    headers: headers
+
+    //Aqui inicia função then
+  }).then(response => {
+
+    if(response.ok) {
+
+      //Esta linha imprime a mensagem no concole
+      console.log('Foi no servidor e voltou');
+
+      //Esta linha carrega a página sucesso
+      window.location.href = 'sucesso.html'    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
+
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+   
+
 }
 
-function capturarDescricaoConta() {
- const descricao_conta = Number(document.getElementById("descricao_conta").value); 
- return descricao_conta;
+function alterar() {
+  const nome_conta = document.getElementById('nome_conta').value;
+  const descricao_conta = document.getElementById('descricao_conta').value;
+
+  console.log(nome_conta)
+  console.log(descricao_conta)
+ 
+  // Cabeçalho não visivel para o usuario
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  fetch('localhost:8080/fluxo/inserir' ,{
+
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+   
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: nome_conta }),
+
+    headers: headers
+
+    //Aqui inicia função then
+  }).then(response => {
+
+    if(response.ok) {
+
+      //Esta linha imprime a mensagem no concole
+      console.log('Foi no servidor e voltou');
+
+      //Esta linha carrega a página sucesso
+      window.location.href = 'sucesso.html'    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
+
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+   
+
 }
 
+function apagar() {
+  const nome_conta = document.getElementById('nome_conta').value;
+  const descricao_conta = document.getElementById('descricao_conta').value;
 
+  console.log(nome_conta)
+  console.log(descricao_conta)
+ 
+  // Cabeçalho não visivel para o usuario
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  fetch('localhost:8080/fluxo/inserir' ,{
+
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+   
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: nome_conta }),
+
+    headers: headers
+
+    //Aqui inicia função then
+  }).then(response => {
+
+    if(response.ok) {
+
+      //Esta linha imprime a mensagem no concole
+      console.log('Foi no servidor e voltou');
+
+      //Esta linha carrega a página sucesso
+      window.location.href = 'sucesso.html'    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
+
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+   
+
+}
--- script/fluxo.js
+++ script/fluxo.js
@@ -1,5 +1,11 @@
 function salvar() {
-  const fluxo = document.getElementById('data_fluxo');
+  const data_fluxo = document.getElementById('data_fluxo').value;
+  const valor_fluxo = document.getElementById('valor_fluxo').value;
+  const descricao_fluxo = document.getElementById('descricao_fluxo').value;
+
+  console.log(data_fluxo)
+  console.log(valor_fluxo)
+  console.log(descricao_fluxo)
  
   // Cabeçalho não visivel para o usuario
   var headers = new Headers();    
@@ -14,7 +20,7 @@ function salvar() {
    
     // Convertendo o objeto JavaScript para JSON
     // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
-    body: JSON.stringify({ nome: fluxo }),
+    body: JSON.stringify({ nome: data_fluxo }),
 
     headers: headers
 
@@ -43,3 +49,155 @@ function salvar() {
 
 }
     
+function consultar() {
+  const data_fluxo = document.getElementById('data_fluxo').value;
+  const valor_fluxo = document.getElementById('valor_fluxo').value;
+  const descricao_fluxo = document.getElementById('descricao_fluxo').value;
+
+  console.log(data_fluxo)
+  console.log(valor_fluxo)
+  console.log(descricao_fluxo)
+ 
+  // Cabeçalho não visivel para o usuario
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  fetch('localhost:8080/fluxo/inserir' ,{
+
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+   
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: data_fluxo }),
+
+    headers: headers
+
+    //Aqui inicia função then
+  }).then(response => {
+
+    if(response.ok) {
+
+      //Esta linha imprime a mensagem no concole
+      console.log('Foi no servidor e voltou');
+
+      //Esta linha carrega a página sucesso
+      window.location.href = 'sucesso.html'    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
+
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+   
+
+}
+
+function alterar() {
+  const data_fluxo = document.getElementById('data_fluxo').value;
+  const valor_fluxo = document.getElementById('valor_fluxo').value;
+  const descricao_fluxo = document.getElementById('descricao_fluxo').value;
+
+  console.log(data_fluxo)
+  console.log(valor_fluxo)
+  console.log(descricao_fluxo)
+ 
+  // Cabeçalho não visivel para o usuario
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  fetch('localhost:8080/fluxo/inserir' ,{
+
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+   
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: data_fluxo }),
+
+    headers: headers
+
+    //Aqui inicia função then
+  }).then(response => {
+
+    if(response.ok) {
+
+      //Esta linha imprime a mensagem no concole
+      console.log('Foi no servidor e voltou');
+
+      //Esta linha carrega a página sucesso
+      window.location.href = 'sucesso.html'    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
+
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+   
+
+}
+
+function apagar() {
+  const data_fluxo = document.getElementById('data_fluxo').value;
+  const valor_fluxo = document.getElementById('valor_fluxo').value;
+  const descricao_fluxo = document.getElementById('descricao_fluxo').value;
+
+  console.log(data_fluxo)
+  console.log(valor_fluxo)
+  console.log(descricao_fluxo)
+ 
+  // Cabeçalho não visivel para o usuario
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+
+  fetch('localhost:8080/fluxo/inserir' ,{
+
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+   
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: data_fluxo }),
+
+    headers: headers
+
+    //Aqui inicia função then
+  }).then(response => {
+
+    if(response.ok) {
+
+      //Esta linha imprime a mensagem no concole
+      console.log('Foi no servidor e voltou');
+
+      //Esta linha carrega a página sucesso
+      window.location.href = 'sucesso.html'    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
+
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
+   
+
+}
